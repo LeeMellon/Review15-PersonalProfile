cbe35b6a312f3f14eb1c539c7a4222645a5ba0dd
Joel Adams <joeladamsdesign@gmail.com>
2018-05-11 09:06:36 -0700

    add repo and EV class

---
+++ Review15-PersonalProfolio/Models/EV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Review15PersonalProfolio.Models
+{
+    public class EV
+    {
+        public static string Key = "4994d53b42facc3a4dd1f4a0676fc0090543596";
+    }
+}
