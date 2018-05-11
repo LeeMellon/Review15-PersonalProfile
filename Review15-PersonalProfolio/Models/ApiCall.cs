26c5fbb9e59583abd3b4ab392ba74d355a32a317
Joel Adams <joeladamsdesign@gmail.com>
2018-05-11 10:09:07 -0700

    add ApiCalls model and refactored Repo model

---
+++ Review15-PersonalProfolio/Models/ApiCall.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Review15PersonalProfolio.Models;
+
+namespace Review15PersonalProfolio.Models
+{
+    public class ApiCall
+    {
+
+
+        public List<Repo> GetRepos()
+        {
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("users/LeeMellon/repos", Method.GET);
+            request.AddHeader("User-Agent", EV.Key);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetRestResponceContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+            var results = JsonConvert.DeserializeObject<List<Repo>>(jsonResponse.ToString());
+            //Console.WriteLine(results[0].Name);
+            //Console.WriteLine(results[1].Name);
+            //Console.WriteLine(results[2].Name);
+            //Console.WriteLine(results[3].Name);
+            //Console.ReadLine();
+
+            return results;
+        }
+
+        public static Task<IRestResponse> GetRestResponceContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, responce =>
+            {
+                tcs.SetResult(responce);
+            });
+            return tcs.Task;
+        }
+    }
+}
--- Review15-PersonalProfolio/Models/Repo.cs
+++ Review15-PersonalProfolio/Models/Repo.cs
@@ -1,15 +1,12 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using RestSharp;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Review15PersonalProfolio.Models
 {
-    [Table("Repos")]
+ 
     public class Repo
     {
         public int Id { get; set; }
@@ -17,44 +14,6 @@ namespace Review15PersonalProfolio.Models
         public string Html_url { get; set; }
         public string Description { get; set; }
         public string Commits_url { get; set; }
-
-        public Repo()
-        {
-
-        }
-
-        public List<Repo>GetRepos()
-        {
-            var client = new RestClient("https://api.github.com");
-            var request = new RestRequest("users/LeeMellon/repos", Method.GET);
-            request.AddHeader("User-Agent", EV.Key);
-            var response = new RestResponse();
-
-            Task.Run(async () =>
-            {
-                response = await GetRestResponceContentAsync(client, request) as RestResponse;
-            }).Wait();
-
-            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
-            var results = JsonConvert.DeserializeObject<List<Repo>>(jsonResponse.ToString());
-            Console.WriteLine(results[0].Name);
-            Console.WriteLine(results[1].Name);
-            Console.WriteLine(results[2].Name);
-            Console.WriteLine(results[3].Name);
-            Console.ReadLine();
-
-            return results;
-        }
-
-        public static Task<IRestResponse> GetRestResponceContentAsync(RestClient theClient, RestRequest theRequest)
-        {
-            var tcs = new TaskCompletionSource<IRestResponse>();
-            theClient.ExecuteAsync(theRequest, responce =>
-            {
-                tcs.SetResult(responce);
-            });
-            return tcs.Task;
-        }
 
     }
 
